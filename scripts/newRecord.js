96a0d3d53fa23ad8c72a720ad3f5e954876ba992
ajt <48979824+ankitjt@users.noreply.github.com>
2022-06-29 11:54:06 +0530

    Add files via upload

---
+++ scripts/newRecord.js
@@ -0,0 +1,29 @@
+let nrpType = document.querySelector(".nrpType"),
+    nrpFirstName = document.querySelector(".nrpFirstName"),
+    nrpLastName = document.querySelector(".nrpLastName"),
+    nrpEmail = document.querySelector(".nrpEmail"),
+    nrpAddress = document.querySelector(".nrpAddress"),
+    nrpMobileNumber = document.querySelector(".nrpMobileNumber"),
+    nrpStartTime = document.querySelector(".nrpStartTime"),
+    nrpRegister = document.querySelector(".nrpRegister"),
+    nrpForm = document.querySelector(".nrpForm"),
+    successNotification = document.querySelector(".successNotification")
+    failNotification = document.querySelector(".failNotification")
+
+nrpRegister.onclick = (e) => {
+    e.preventDefault();
+    if(nrpType.value === "" || nrpFirstName.value === "" || nrpLastName.value=== "" || nrpEmail.value === "" || nrpAddress.value=== "" || nrpMobileNumber.value === "" || nrpStartTime.value === "") {
+        failNotification.classList.remove("hidden")
+        failNotification.classList.add("block")
+        successNotification.classList.remove("block")
+        successNotification.classList.add("hidden")
+    }
+    else {
+        console.table([nrpType.value, nrpFirstName.value, nrpLastName.value, nrpEmail.value, nrpMobileNumber.value, nrpAddress.value, nrpStartTime.value])
+        failNotification.classList.remove("block")
+        failNotification.classList.add("hidden")
+        successNotification.classList.add("block")
+        successNotification.classList.remove("hidden")
+        nrpForm.reset()
+    }
+}
