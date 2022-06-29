8ab1f4dae3e8cd732bae11eb1b79aee5c5644da5
ajt <48979824+ankitjt@users.noreply.github.com>
2022-06-29 16:06:07 +0530

    Add files via upload

--- scripts/newRecord.js
+++ scripts/newRecord.js
@@ -6,20 +6,21 @@ let nrpType = document.querySelector(".nrpType"),
     nrpMobileNumber = document.querySelector(".nrpMobileNumber"),
     nrpStartTime = document.querySelector(".nrpStartTime"),
     nrpRegister = document.querySelector(".nrpRegister"),
+    nrpFee = document.querySelector(".nrpFee"),
     nrpForm = document.querySelector(".nrpForm"),
     successNotification = document.querySelector(".successNotification")
     failNotification = document.querySelector(".failNotification")
 
 nrpRegister.onclick = (e) => {
     e.preventDefault();
-    if(nrpType.value === "" || nrpFirstName.value === "" || nrpLastName.value=== "" || nrpEmail.value === "" || nrpAddress.value=== "" || nrpMobileNumber.value === "" || nrpStartTime.value === "") {
+    if(nrpType.value === "" || nrpFirstName.value === "" || nrpLastName.value=== "" || nrpEmail.value === "" || nrpAddress.value=== "" || nrpMobileNumber.value === "" || nrpStartTime.value === "" || nrpFee.value === "") {
         failNotification.classList.remove("hidden")
         failNotification.classList.add("block")
         successNotification.classList.remove("block")
         successNotification.classList.add("hidden")
     }
     else {
-        console.table([nrpType.value, nrpFirstName.value, nrpLastName.value, nrpEmail.value, nrpMobileNumber.value, nrpAddress.value, nrpStartTime.value])
+        console.table([nrpType.value, nrpFirstName.value, nrpLastName.value, nrpEmail.value, nrpMobileNumber.value, nrpAddress.value, nrpStartTime.value, nrpFee.value])
         failNotification.classList.remove("block")
         failNotification.classList.add("hidden")
         successNotification.classList.add("block")
